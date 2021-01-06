30f23fd5db8b0ac33d8e4f4df77deaafb0ff911c
김유빈 <luwbe1@naver.com>
2021-01-06 23:15:45 +0900

    django course 28 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
@@ -5,12 +6,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+has_ownership = [account_ownership_required,login_required]
+
+
+@login_required
 def hello_world(request):
 
     if request.method == "POST":
@@ -33,12 +40,15 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')  # reverse_lazy는 클래스형 뷰에서 사용한다.
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -46,8 +56,12 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')  # reverse_lazy는 클래스형 뷰에서 사용한다.
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')  # reverse_lazy는 클래스형 뷰에서 사용한다.
     template_name = 'accountapp/delete.html'
+
