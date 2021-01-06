f72b0a78e12a2902a0769150fdb99e92e4db6021
김유빈 <luwbe1@naver.com>
2021-01-07 00:35:07 +0900

    django course 30 commit

---
+++ profileapp/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True)  # 프로필 사진
+    nickname = models.CharField(max_length=20, unique=True, null=True)  # 닉네임
+    message = models.CharField(max_length=100, null=True)  # 메세지
+    
+
