84c5ee3367002a0e082355fc22ea8eae54d26d8a
김유빈 <luwbe1@naver.com>
2021-01-12 03:39:47 +0900

    django course 42 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -31,9 +32,15 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
-        object_list = Article.objects.filter(project=self.get_object())
+        project = self.object
+        user = self.request.user
 
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, subscription=subscription,
+                                                               **kwargs)
 
 
 class ProjectListView(ListView):
@@ -41,4 +48,3 @@ class ProjectListView(ListView):
     context_object_name = 'project_list'
     template_name = 'projectapp/list.html'
     paginate_by = 25
-
